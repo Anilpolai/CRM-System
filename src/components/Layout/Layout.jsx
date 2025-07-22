cf0bacb9b696f280d0cc89807ef7270d7ece94f2
Anilpolai <anil.polai03@gmail.com>
2025-07-23 04:41:22 +0530

    add new css sidebar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Navbar from '../Navber/navbar';
-import Sidebar from '../sidebar';
+import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
