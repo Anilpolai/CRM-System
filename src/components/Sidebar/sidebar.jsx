bf65baf9f2b108d9fff8380fe03a1e12e40887ee
Anilpolai <anil.polai03@gmail.com>
2025-07-23 05:11:04 +0530

    push now nwe

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -1,16 +1,24 @@
-// Sidebar.jsx
+// src/components/Sidebar.jsx
 import React from 'react';
 import { BiHomeAlt, BiUser, BiChat, BiCog } from 'react-icons/bi';
-import './sidebar.css';
+import './sidebar.css'; // Make sure this file name is all lowercase
 
 const Sidebar = ({ isOpen }) => {
   return (
     <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
       <ul className="list-unstyled p-3 m-0">
-        <li><BiHomeAlt size={20} /> <span>Home</span></li>
-        <li><BiUser size={20} /> <span>Users</span></li>
-        <li><BiChat size={20} /> <span>Messages</span></li>
-        <li><BiCog size={20} /> <span>Settings</span></li>
+        <li>
+          <BiHomeAlt size={20} /> <span>Home</span>
+        </li>
+        <li>
+          <BiUser size={20} /> <span>Users</span>
+        </li>
+        <li>
+          <BiChat size={20} /> <span>Messages</span>
+        </li>
+        <li>
+          <BiCog size={20} /> <span>Settings</span>
+        </li>
       </ul>
     </div>
   );
