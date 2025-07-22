4da6c99c81209162f20def7c6cebfe0decec6c3b
Anilpolai <anil.polai03@gmail.com>
2025-07-23 02:21:54 +0530

    add the navber and sidebar

---
+++ src/components/Navber/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './navbar.css'
+const navbar = () => {
+  return (
+    <>
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <h1>CRM System</h1> 
+            </div>
+            </nav>
+    </>
+  )
+}
+
+export default navbar
