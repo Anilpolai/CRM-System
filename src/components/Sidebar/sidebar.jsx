87c3fb69187debf327dd79dd67b16445b9a0cf0a
Anilpolai <anil.polai03@gmail.com>
2025-07-23 04:59:31 +0530

    change sidbar code

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -1,16 +1,16 @@
 // Sidebar.jsx
 import React from 'react';
 import { BiHomeAlt, BiUser, BiChat, BiCog } from 'react-icons/bi';
-import './sidebar.css'; // Assuming you have a CSS file for styling
+import './Sidebar.css'; 
 
 const Sidebar = ({ isOpen }) => {
   return (
     <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
-      <ul className="list-unstyled p-3">
-        <li><BiHomeAlt /> <span>Home</span></li>
-        <li><BiUser /> <span>Users</span></li>
-        <li><BiChat /> <span>Messages</span></li>
-        <li><BiCog /> <span>Settings</span></li>
+      <ul className="list-unstyled p-3 m-0">
+        <li><BiHomeAlt size={20} /> <span>Home</span></li>
+        <li><BiUser size={20} /> <span>Users</span></li>
+        <li><BiChat size={20} /> <span>Messages</span></li>
+        <li><BiCog size={20} /> <span>Settings</span></li>
       </ul>
     </div>
   );
